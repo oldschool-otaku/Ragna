d124340909c673726318404b95fc1c9cdcb32177
denamo <artstarxd@outlook.com>
2022-03-25 15:57:55 +0300

    начинаю переебашивать ИИ

--- Mechanics/AI.cs
+++ Mechanics/AI.cs
@@ -10,17 +10,42 @@ public class AI
     {
         if (Boss.IsDead() is not false) return;
         if (Gameplay.ThrowTheDice() < 3)
+        {
             Boss.GetSelfHealed();
-        else
-            Boss.AttackPlayer(Boss, player);
+            return;
+        }
+        Boss.AttackPlayer(Boss, player);
     }
 
     public static void HealChoice(Character Boss, Player player)
     {
         if (Boss.IsDead() is not false) return;
         if (Gameplay.ThrowTheDice() < 9)
+        {
             Boss.GetSelfHealed();
-        else
+            return;
+        }
+        Boss.AttackPlayer(Boss, player);
+    }
+
+    public static void Heal(Character Boss, Player player, int prev_pick)
+    {
+        if (Boss.IsDead() is not false) return;
+        if (Boss.Health > Boss.Health / 4 
+            && prev_pick != 3 
+            && player.Health > player.Health / 4) return;
+        Boss.GetSelfHealed();
+    }
+
+    public static void FinishPlayer(Character Boss, Player player)
+    {
+        if (Boss.IsDead() is not false) return;
+        if (player.Health <= player.Health / 4)
             Boss.AttackPlayer(Boss, player);
+    }
+
+    public static void gfhgddfghg(Character Boss, Player player)
+    {
+        throw new NotImplementedException();
     }
 }
